d5f52fc2f01377b6bdeb7122a0e8aeb168a4a368
mat kelcey <matthew.kelcey@gmail.com>
2011-12-03 15:54:00 -0800

    weird problem with define? avoid by having explicit class

---
+++ java/src/cc/udfs/Bigrams.java
@@ -0,0 +1,8 @@
+package cc.udfs;
+
+//having problems with DEFINE so avoid by having explicit class for now
+public class Bigrams extends Ngrams {
+  public Bigrams() {
+    super(2,false);
+  }
+}
---
+++ java/src/cc/udfs/Unigrams.java
@@ -0,0 +1,8 @@
+package cc.udfs;
+
+// having problems with DEFINE so avoid by having explicit class for now
+public class Unigrams extends Ngrams {
+  public Unigrams() {
+    super(1,false);
+  }
+}
